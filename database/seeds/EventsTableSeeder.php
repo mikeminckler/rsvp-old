acb9d02847c7b071ac7e577025b2aa86de808845
Mike Minckler <mikeminckler@gmail.com>
2019-12-20 10:34:23 -0800

    updated event seeder for 2019 events

--- database/seeds/EventsTableSeeder.php
+++ database/seeds/EventsTableSeeder.php
@@ -16,8 +16,8 @@ class EventsTableSeeder extends Seeder
 
         $event = new Event;
         $event->title = 'Whistler';
-        $event->start_date = '2020-02-10 09:00:00';
-        $event->end_date = '2020-02-10 17:00:00';
+        $event->start_date = '2020-01-28 09:00:00';
+        $event->end_date = '2020-01-28 17:00:00';
         $event->location = 'One on One Meetings';
         $event->host_name = 'Crystal Harvey';
         $event->host_email = 'crystal.harvey@brentwood.bc.ca';
@@ -26,8 +26,8 @@ class EventsTableSeeder extends Seeder
 
         $event = new Event;
         $event->title = 'West Vancouver';
-        $event->start_date = '2020-02-11 9:00:00';
-        $event->end_date = '2020-02-11 17:00:00';
+        $event->start_date = '2020-01-29 9:00:00';
+        $event->end_date = '2020-01-29 17:00:00';
         $event->location = 'One on One Meetings';
         $event->host_name = 'Crystal Harvey';
         $event->host_email = 'crystal.harvey@brentwood.bc.ca';
